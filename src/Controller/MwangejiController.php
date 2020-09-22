8720ed7d14bf828a36882ef1e4a8dcdbcfa24f74
jessy45 <jessymukund@gmail.com>
2020-09-22 13:02:58 +0200

    initial commit

---
+++ src/Controller/MwangejiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MwangejiController extends AbstractController
+{
+    /**
+     * @Route("/mwangeji", name="mwangeji")
+     */
+    public function index()
+    {
+        return $this->render('mwangeji/index.html.twig', [
+            'controller_name' => 'MwangejiController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="login")
+    */
+    public function login()
+    {
+        return $this->render('/mwangeji/login.html.twig',
+        ['title'=>'Connectez-vous']
+        );
+    }
+}
